17b119cf14e88047adbc11c185e4e396460667d4
Juan Rosario <juanelias18@hotmail.com>
2019-09-21 19:11:10 -0400

    Repositorio empezado

---
+++ BLL/RepositorioBase.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class RepositorioBase : IDisposable, IRepository<T> where T : class
+    {
+        internal Contexto contexto;
+
+        public RepositorioBase()
+        {
+            contexto = new Contexto();
+        }
+
+        public virtual void Dispose()
+        {
+            contexto.Dispose();
+        }
+    }
+}
